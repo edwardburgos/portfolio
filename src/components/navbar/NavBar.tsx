2f81235994af4db8492bd363518bfd4c874ab215
EdwardBurgos <edwardpbn@gmail.com>
2021-10-26 19:46:21 -0500

    implemented sidebar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -6,34 +6,84 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import s from './NavBar.module.css'
 import NightsStayIcon from '@mui/icons-material/NightsStay';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import ListItemText from '@mui/material/ListItemText';
+import FolderOutlinedIcon from '@mui/icons-material/FolderOutlined';
+import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
 
 export default function NavBar() {
 
+  const [showDrawer, setShowDrawer] = useState(false)
+
+
+  const toggleDrawer =
+    (open: boolean) =>
+      (event: React.KeyboardEvent | React.MouseEvent) => {
+        if (
+          event.type === 'keydown' &&
+          ((event as React.KeyboardEvent).key === 'Tab' ||
+            (event as React.KeyboardEvent).key === 'Shift')
+        ) return;
+        setShowDrawer(open);
+      };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed">
-        <Toolbar className={s.toolBar}>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            className={s.buttonMenu}
-          >
-            <MenuIcon fontSize="small" />
-          </IconButton>
-          <p className={s.title}>Edward Burgos</p>
-          <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: 'flex'} }}>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="fixed">
+          <Toolbar className={s.toolBar}>
             <IconButton
-              aria-label="show more"
               color="inherit"
-              className={s.buttonMore}
-              onClick={() => document.body.classList.toggle("dark-mode")}
+              aria-label="open drawer"
+              className={s.buttonMenu}
+              onClick={toggleDrawer(true)}
             >
-              <NightsStayIcon fontSize="small" />
+              <MenuIcon fontSize="small" />
             </IconButton>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
+            <p className={s.title}>Edward Burgos</p>
+            <Box sx={{ flexGrow: 1 }} />
+            <Box sx={{ display: { xs: 'flex' } }}>
+              <IconButton
+                aria-label="show more"
+                color="inherit"
+                className={s.buttonMore}
+                onClick={() => document.body.classList.toggle("dark-mode")}
+              >
+                <NightsStayIcon fontSize="small" />
+              </IconButton>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <Drawer
+        anchor='left'
+        open={showDrawer}
+        onClose={toggleDrawer(false)}
+      >
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+        >
+          <List>
+            {['Home', 'Skills', 'Portfolio'].map(text => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  { text === 'Home' ? <HomeOutlinedIcon /> : text === 'Skills' ? <LightbulbOutlinedIcon /> : <FolderOutlinedIcon /> }
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+
+        </Box>
+      </Drawer>
+    </>
   );
 }
